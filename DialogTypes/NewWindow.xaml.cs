839dd36362ccc86e4eb33db40bd03f0e944e5173
Николай Никоноров <nnv@nikolay.moe>
2021-10-02 14:18:30 +0300

    init/Задание 1

---
+++ DialogTypes/NewWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace DialogTypes
+{
+    public partial class NewWindow : Window
+    {
+        public NewWindow()
+        {
+            InitializeComponent();
+        }
+
+        public NewWindow(string title)
+        {
+            InitializeComponent();
+            Title = title;
+        }
+    }
+}
