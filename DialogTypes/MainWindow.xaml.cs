076afda3c03f863fc25a4fa6d8c1f3b93685c8e3
Николай Никоноров <nnv@nikolay.moe>
2021-10-02 14:22:11 +0300

    done/Задание 1

--- DialogTypes/MainWindow.xaml.cs
+++ DialogTypes/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace DialogTypes
+﻿using System.Windows;
+
+namespace DialogTypes
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -9,5 +11,18 @@
         {
             InitializeComponent();
         }
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            NewWindow window = new NewWindow("Модальное");
+            window.ShowDialog();
+            // Код после метода ShowDialog выполнится только тогда, кода диалоговое окно закроется.
+        }
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            NewWindow window = new NewWindow("Немодальное");
+            window.Show();
+            // Код после метода Show выполнится сразу.
+        }
+
     }
 }
