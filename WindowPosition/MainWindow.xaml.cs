bf3ee3bf114b4676d479657f4ee5ac26e31c2f2f
Николай Никоноров <nnv@nikolay.moe>
2021-10-02 15:04:25 +0300

    done/Задание 3

--- WindowPosition/MainWindow.xaml.cs
+++ WindowPosition/MainWindow.xaml.cs
@@ -11,6 +11,20 @@ namespace WindowPosition
         public MainWindow()
         {
             InitializeComponent();
+            this.WindowStyle = System.Windows.WindowStyle.ToolWindow;
         }
+        // Для Window в XAML коде WindowStartupLocation="CenterScreen" для старта окна в центре экрана.
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            // Вариант 1
+            Double width  = SystemParameters.FullPrimaryScreenWidth;
+            Double height = SystemParameters.FullPrimaryScreenHeight;
+            //// Вариант 2 не учитывается панель задач.
+            //width = SystemParameters.WorkArea.Width;
+            //height = SystemParameters.WorkArea.Height;
+            this.Top = (height - this.Height) / 2;
+            this.Left = (width - this.Width) / 2;
+        }
+
     }
 }
