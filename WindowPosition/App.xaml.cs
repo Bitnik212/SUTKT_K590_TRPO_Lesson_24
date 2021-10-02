84b1c2b3b9785da8c8f367f260ba5a11aaf39302
Николай Никоноров <nnv@nikolay.moe>
2021-10-02 15:04:10 +0300

    init/Задание 3

---
+++ WindowPosition/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WindowPosition
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ WindowPosition/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+
+namespace WindowPosition
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
