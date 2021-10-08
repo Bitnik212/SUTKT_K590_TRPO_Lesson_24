2afcf1deb26ae86cc33bfd1589ca014658847a8c
Николай Никоноров <nnv@nikolay.moe>
2021-10-08 08:22:12 +0300

    init/Задание 5

---
+++ WindowInteracting/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WindowInteracting
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ WindowInteracting/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace WindowInteracting
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
