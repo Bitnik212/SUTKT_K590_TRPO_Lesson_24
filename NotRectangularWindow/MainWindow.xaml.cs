dbade180ea8108ed51300fdf7abf4f8e1b2a4424
Николай Никоноров <nnv@nikolay.moe>
2021-10-08 08:50:35 +0300

    init/Задание 8

---
+++ NotRectangularWindow/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace NotRectangularWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
