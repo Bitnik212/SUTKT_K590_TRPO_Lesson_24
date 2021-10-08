f8be4a9a7b00602cafce0e6c299d74d375e7e429
Николай Никоноров <nnv@nikolay.moe>
2021-10-08 08:49:36 +0300

    done/Задание 7

--- SaveOpenFileDialogs/MainWindow.xaml.cs
+++ SaveOpenFileDialogs/MainWindow.xaml.cs
@@ -1,13 +1,43 @@
-﻿namespace SaveOpenFileDialogs
+﻿using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace SaveOpenFileDialogs
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow
     {
+        // Из пространства имен Microsoft.Win32
+        OpenFileDialog _openDialog = new OpenFileDialog();
+        SaveFileDialog _saveDialog = new SaveFileDialog();
+
         public MainWindow()
         {
             InitializeComponent();
         }
+        private void buttonSave_Click(object sender, RoutedEventArgs e)
+        {
+            // Настраиваем диалоговое окно для сохранения файлов. Указываем два фильтра для расширений файлов.
+            _saveDialog.Filter = "Text files (*.TXT)|*.txt|All Files (*.*)|*.*";
+            if (_saveDialog.ShowDialog() == true)
+            {
+                StreamWriter writer = new StreamWriter(_saveDialog.FileName);
+                writer.WriteLine(textBoxContent.Text);
+                writer.Close();
+            }
+        }
+        private void buttonOpen_Click(object sender, RoutedEventArgs e)
+        {
+            _openDialog.Filter = "Text files (*.TXT)|*.txt|All Files (*.*)|*.*";
+            if (_openDialog.ShowDialog() == true)
+            {
+                StreamReader reader = new StreamReader(_openDialog.FileName);
+                textBoxContent.Text = reader.ReadToEnd();
+                reader.Close();
+            }
+        }
+
     }
 }
