0e6cf646337315b2005ad9a8f0fb55c769dcc33a
Николай Никоноров <nnv@nikolay.moe>
2021-10-08 09:21:12 +0300

    done/Вариант 2

--- Variant_2/MainWindow.xaml.cs
+++ Variant_2/MainWindow.xaml.cs
@@ -1,13 +1,26 @@
-﻿namespace Variant_2
+﻿using System.Windows;
+using Microsoft.Win32;
+
+namespace Variant_2
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow
     {
-        public MainWindow()
-        {
-            InitializeComponent();
+        ExtraWindow extraWindow = new ExtraWindow();
+        OpenFileDialog opener = new OpenFileDialog();
+
+        public MainWindow() => InitializeComponent();
+
+        void add(object sender, RoutedEventArgs e){
+            opener.Filter = "Image files (*.PNG)|*.png|All Files (*.*)|*.*";
+            if(opener.ShowDialog() == true)
+                MessageBox.Show(extraWindow.add(opener.FileName));
         }
+
+        void show(object sender, RoutedEventArgs e) => extraWindow.ShowDialog();
+
+        void shutdown(object sender, System.ComponentModel.CancelEventArgs e) => App.Current.Shutdown();
     }
 }
