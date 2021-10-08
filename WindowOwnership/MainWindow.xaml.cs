c005238e039e8ed979f990c60cc6f178d4db14a5
Николай Никоноров <nnv@nikolay.moe>
2021-10-08 08:47:29 +0300

    done/Задание 6

--- WindowOwnership/MainWindow.xaml.cs
+++ WindowOwnership/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace WindowOwnership
+﻿using System.Windows;
+
+namespace WindowOwnership
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -9,5 +11,15 @@
         {
             InitializeComponent();
         }
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            // Окна, имеющие окно-владельца, удобно применять для всплывающих окон и панелей инструментов.
+            // Дочернее окно всегда отображается поверх своего владельца и сворачивается, когда сворачивается окно-владелец.
+            Window1 window = new Window1();
+            window.Owner = this;
+            window.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            window.Show();
+        }
+
     }
 }
---
+++ WindowOwnership/Window1.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace WindowOwnership
+{
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            (Owner as MainWindow).Title = "Hello from child"; // получение ссылки на родительское окно
+        }
+    }
+}
