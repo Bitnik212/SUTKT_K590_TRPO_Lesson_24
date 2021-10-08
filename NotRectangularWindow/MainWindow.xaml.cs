808af09837dbbf1bde950db6c7c9282b1ee7d102
Николай Никоноров <nnv@nikolay.moe>
2021-10-08 09:04:10 +0300

    done/Задание 8. Типа сделал

--- NotRectangularWindow/MainWindow.xaml.cs
+++ NotRectangularWindow/MainWindow.xaml.cs
@@ -1,13 +1,33 @@
-﻿namespace NotRectangularWindow
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace NotRectangularWindow
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow
     {
+        /*
+          * 1. Задать свойство AllowTransparency = true
+          * 2. Установить WindowsStyle = None
+          * 3. В качестве фона установить картинку с прозрачными элементами.
+          */
+
         public MainWindow()
         {
             InitializeComponent();
         }
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            // Закрываем текущее приложение.
+            Application.Current.Shutdown();
+        }
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // Для перетаскивания окна за любую область.
+            DragMove();
+        }
+
     }
 }
---
+++ NotRectangularWindow/MainWindow2.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace NotRectangularWindow
+{
+    public partial class MainWindow2 : Window
+    {
+        public MainWindow2()
+        {
+            InitializeComponent();
+        }
+        private void buttonClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+    }
+}
